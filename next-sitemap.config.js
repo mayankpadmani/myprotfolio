27ba0d395f8d78db3769674e77473378459e89b5
Mayank Padmani <padmanimayank12@gmail.com>
2026-07-16 15:09:36 +0530

    index

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,10 @@ module.exports = {
   siteUrl: 'https://www.mayankpadmani.me',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  exclude: ['/api/*'],
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+    additionalSitemaps: ['https://www.mayankpadmani.me/sitemap.xml'],
+  },
 };
     
